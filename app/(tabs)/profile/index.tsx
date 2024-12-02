c01cee600e70dc2be3be88dc938ce8f1f2de2575
Khant Thit Oo <127503879+travthegoat@users.noreply.github.com>
2024-12-02 21:11:52 +0630

    ahm ahm

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,18 +1,42 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import * as SecureStore from 'expo-secure-store';
 import { router } from "expo-router";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import { fetchData } from "@/hooks/api";
+
+interface UserData {
+    name: string;
+}
 
 const ProfilePage = () => {
+    const [username, setUsername] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const getData = async () => {
+            const uid = await SecureStore.getItemAsync('uid');
+            setLoading(true);
+            try {
+                const result = await fetchData(`/users/${uid}`);
+                console.log(result);
+                setUsername(result.username);
+            } catch (err) {
+                console.log(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getData();
+    }, [])
+
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={async () => {
-                await SecureStore.deleteItemAsync('uid');
-                router.replace('/');
-            }}>
-                <Text style={{ textAlign: "center", fontSize: 40 }}>
-                    Log out
-                </Text>
-            </TouchableOpacity>
+            <View style={styles.titleContainer}>
+                <MaterialCommunityIcons name="face-man-profile" size={150} color="black" />
+                <Text style={styles.titleText}>{username}</Text>
+            </View>
         </View>
     );
 };
@@ -20,9 +44,17 @@ const ProfilePage = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignContent: "center",
     },
+    titleContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: 70,
+    },
+    titleText: {
+        fontSize: 24,
+        fontWeight: 'semibold',
+    }
 });
 
 export default ProfilePage;
