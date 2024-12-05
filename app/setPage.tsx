370d256461e5bd303a6aed947aa65ce825c3560f
Nokimishi <143719405+Nokimishi@users.noreply.github.com>
2024-12-05 14:34:49 +0630

    Progress on feature_setpage dec5

--- app/setPage.tsx
+++ app/setPage.tsx
@@ -9,15 +9,27 @@ import {
 import { AntDesign } from "@expo/vector-icons";
 import { router } from "expo-router";
 
-const DATA = [
-    { id: '1', title: 'Item 1' },
-    { id: '2', title: 'Item 2' },
-    { id: '3', title: 'Item 3' },
-    { id: '4', title: 'Item 4' },
-    { id: '5', title: 'Item 5' },
-];
-
 const SetPage = () => {
+    const data = [
+        { id: '1', title: 'Card 1', description: 'Bla bla bla 34',type: 'text1'},
+        { id: '2', title: 'Card 2', description: 'Bla bla blav55',type: 'text2' },
+        { id: '3', title: 'Card 3', description: 'Bla bla bla6565',type: 'text3' },
+        { id: '4', title: 'Card 4', description: 'Bla bla bla 78',type: 'text4' }, // Added a fourth card for demonstration
+    ];
+
+    const renderItem = ({ item }) => (
+    <View style={styles.card}>
+        <View style={styles.cardHeader}>
+            <Text style={styles.cardTitle}>{item.title}</Text>
+            <TouchableOpacity>
+                <AntDesign name="ellipsis1" size={20} color="black" />
+            </TouchableOpacity>
+        </View>
+        <Text style={styles.cardDescription}>{item.description}</Text>
+        <Text style={styles.cardDescription}>{item.type}</Text>
+    </View>
+    );
+
     return (
         <>
             <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent={true} />
@@ -25,15 +37,22 @@ const SetPage = () => {
                 <View style={styles.titleContainer}>
                     <Text style={styles.titleText}>Set Page</Text>
                     <TouchableOpacity style={{ paddingTop: 3 }}
-                    onPress={() =>
-                        router.push({
-                            pathname: "/setPage_createCard",
-                        })
-                    }
+                        onPress={() =>
+                            router.push({
+                                pathname: "/setPage_createCard",
+                            })
+                        }
                     >
                         <AntDesign name="pluscircle" size={28} color="black" />
                     </TouchableOpacity>
                 </View>
+                <FlatList
+                    data={data}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                    numColumns={2}
+                    contentContainerStyle={styles.cardList}
+                />
             </View>
         </>
     );
@@ -56,6 +75,42 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: "bold",
         color: "black",
+    },
+    cardList: {
+        padding: 20,
+    },
+    card: {
+        flex: 1,
+        margin: 10,
+        backgroundColor: 'white',
+        borderTopLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        borderTopRightRadius: 5,
+        borderBottomLeftRadius: 5,
+        padding: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+        elevation: 5,
+    },
+    cardHeader: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        
+    },
+    cardTitle: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#7450ff',
+    },
+    cardDescription: {
+        marginTop: 10,
+        fontSize: 16,
     },
 });
 
