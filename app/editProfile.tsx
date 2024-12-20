f5d5200e1eaf15f5021a7ffddfedc36c1eb19ed3
Khant Thit Oo <127503879+travthegoat@users.noreply.github.com>
2024-12-20 18:06:12 +0630

    wrong move

--- app/editProfile.tsx
+++ app/editProfile.tsx
@@ -1,9 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ViewBase } from "react-native";
+import * as SecureStore from 'expo-secure-store';
+import { fetchData } from "@/hooks/api";
 
 const EditProfilePage: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const getData = async () => {
+        const uid = await SecureStore.getItemAsync('uid');
+        
+        setLoading(true);
+        try {
+            const result = await fetchData(`/users/${uid}`)
+            setEmail(result.email);
+            setUsername(result.username);
+        } catch (err) {
+            console.error(err);
+        }
+
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
-        <View>
-            <Text>Profile Edit</Text>
+        <View style={styles.container}>
+            
+            <View style={styles.contentContainer}>
+
+            </View>
+
         </View>
     );
 };
@@ -11,7 +40,14 @@ const EditProfilePage: React.FC = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 20,
     },
+    contentContainer: {
+        width: '100%',
+        height: '40%',
+        backgroundColor: 'black',
+    }
 });
 
 export default EditProfilePage;
